fd7a24ffcc40eafefde07e6df9a2536712b8c932
laurenr0324 <laurenr0324@gmail.com>
2025-08-31 16:57:44 -0400

    hw2 complete

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code here
+function greet() {
+    var name = prompt("What is your name?")
+    alert("Hello, " + name)
+    var age = parseInt(prompt("How old are you?"))
+    var hadBirthday = confirm("Have you had a birthday this year?")
+    var currentYear = new Date().getFullYear()
+    var birthYear = hadBirthday ? currentYear - age : currentYear - age - 1
+    alert("You were born in " + birthYear)
+}
